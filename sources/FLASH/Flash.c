cabb18e1a7166b9648bca9859195e7b1896fbbec
user <user@ubuntu.(none)>
2013-12-17 09:21:54 -0500

    correcao no ERASE_FLASH

--- sources/FLASH/Flash.c
+++ sources/FLASH/Flash.c
@@ -9,6 +9,29 @@ unsigned int FLASH_EraseSectors(unsigned int startSector, unsigned int endSector
 {
 	iap_entry = (IAP) IAP_ADDR;
 	
+	/* 
+	 * Preparar os comandos a ser enviados para o IAP 
+	 * Sempre que for feita uma operacao de write ou erase é preciso
+	 * preparar os sectores para essa operacao
+	 * */
+	IAP_command[0] = PREPARE_SECTORS_FOR_WRITE_OPERATION;
+	IAP_command[1] = (unsigned int) startSector;
+	IAP_command[2] = (unsigned int) endSector;
+	
+	IAP_entry(IAP_Command, IAP_result);
+	
+	if(IAP_result[0] == CMD_SUCCESS)
+	{
+		/* Preparar os comandos a ser enviados para o IAP */
+		IAP_command[0] = ERASE_SECTORS;
+		IAP_command[1] = (unsigned int) startSector;
+		IAP_command[2] = (unsigned int) endSector;
+	
+		IAP_entry(IAP_Command, IAP_result);
+		return IAP_result[0];
+	}
+	/* Este retorno é da verificação para apagar a flash*/
+	return IAP_result[0];
 }
 
 unsigned int FLASH_WriteBlock( void *dstAddr, void *srcAddr, unsigned int size)
@@ -29,6 +52,7 @@ unsigned int FLASH_VerifyData(void *dstAddr, void *srcAddr, unsigned int size)
 	IAP_command[0] = COMPARE;
 	IAP_command[1] = (unsigned int)dstAddr;
 	IAP_command[2] = (unsigned int)srcAddr;
+	IAP_command[3] = size;
 	
 	iap_entry(IAP_command, IAP_result);
 	
